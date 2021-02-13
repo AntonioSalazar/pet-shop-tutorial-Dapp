9529abeb5190c645c6188211276c745c4f0e9c4b
unknown <antonio.salazar.emrsn@gmail.com>
2021-02-13 01:15:46 -0600

    I have added the getAdopters function to the adoption contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -8,9 +8,15 @@ contract Adoption {
     //Adopting a pet
     //in Solidity both, the parameter of the function and the output type must be specified
     function adopt (uint petId) public returns (uint){
+        //we make sure the petId is in range of our adopters array
         require(petId >= 0 && petId <=15);
 
         adopters[petId] = msg.sender;
         return petId;
+    }
+
+    //Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory){
+        return adopters;
     }
 }
